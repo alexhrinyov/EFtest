9f143ad961d5a31d3cc91fa4dc1628af1239518f
alexhrinyov <hrinyou29@gmail.com>
2023-03-08 22:07:24 +0300

    Add project files.

---
+++ EFtest/AppContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFtest
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблиц
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = DESKTOP-BQ6LNRN\SQLEXPRESS; TrustServerCertificate = true; Database = EFtest; Trusted_Connection = True;");
+        }
+    }
+}
---
+++ EFtest/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EFtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AppContext())
+            {
+                // Использование EF
+                var user1 = new User { Name = "Arthur", Email = "artur@mail.ru" };
+                var book1 = new Book { Title = "Старик и Море", Year = 1951 };
+                db.Users.Add(user1);
+                db.Books.Add(book1);
+                db.SaveChanges();
+                Console.WriteLine("Нажмите любую клавишу...");
+            }
+        }
+    }
+}
